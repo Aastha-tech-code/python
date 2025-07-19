3aff62290e1f6a4bfb3b48b38bf867b715aa7585
Aastha-tech-code <aasthachambial@gmail.com>
2025-07-19 12:09:18 +0530

    hi

---
+++ chapter2/practice.py/7.py
@@ -0,0 +1,10 @@
+name = "Aastha"
+age = 19
+quote = "Believe in yourself, magic happens."
+
+print("Name:", name)
+print("Age:", age)
+print("Favorite Quote:", quote)
+print("Characters in Name:", len(name))
+print("Characters in Quote:", len(quote))
+print("Sum of Age and Characters in Name:", age + len(name))    
